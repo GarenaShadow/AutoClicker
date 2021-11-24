8c6f2331c2d56e5a420b1bcd827230f6f4fda3d9
GarenaShadow <85088817+GarenaShadow@users.noreply.github.com>
2021-11-24 11:59:44 +0800

    Update .lua

--- .lua
+++ .lua
@@ -1,8 +1,9 @@
-# AutoClicker.lua
+--// Services \\--
 local CoreGui = game:GetService("CoreGui")
 local Players = game:GetService("Players")
 local VirtualInputManager = game:GetService("VirtualInputManager")
 
+--// Variables \\--
 local Player = Players.LocalPlayer
 local Enabled = false
 local Mouse = Player:GetMouse()
@@ -10,6 +11,7 @@ local X, Y = 0, 0
 local LastC = Color3.new(1, 0, 0)
 local LastU = tick()
 
+--// Exploit Fix \\--
 if not pcall(function() return syn.protect_gui end) then
     syn = {}
     syn.protect_gui = function(A_1)
@@ -17,11 +19,12 @@ if not pcall(function() return syn.protect_gui end) then
     end
 end
 
+--// UI Library \\--
 local Library = loadstring(game:HttpGetAsync('https://pastebin.com/raw/edJT9EGX'))()
 local Window = Library:CreateWindow("AutoClicker by GGSemidah")
 Enabled_1 = Window:AddColor({
     text = 'Status:',
-    flag = "GGSemidah",
+    flag = "Ezpi_1",
     color = Color3.new(1, 0, 0),
     callback = function(A_1)
         -- "Enabled" Color
